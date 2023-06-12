519764d3f9cd353406a84b064f7156513b887f22
jimao <jimao@jimaodeMacBook-Pro.local>
2023-06-12 18:24:39 +0800

    提交代码

--- src/neck.ts
+++ src/neck.ts
@@ -5,5 +5,6 @@ function route(file_path: string) {
   let route = path.relative(path.join(process.cwd(), 'dist'), file_path).toLowerCase();
   route = path.join('/', route);
   if (route.endsWith('.js')) route = route.slice(0, route.length - 3);
+  console.log('mount', route);
   return route;
 }
