abeae7dae0d9ec895fb97405509251e8760cfc91
jimao <jimao@jimaodeMacBook-Pro.local>
2023-06-13 15:01:15 +0800

    提交代码

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 import nft from './api/my/nft';
 
 const _app = express();
+_app.use(cors());
 _app.use(...nft);
 
 export const app = _app;
